6574b37f49e6e47cd15a90fc20d4e6a5d3f84b72
xiao-wang007 <xwang0505dazzleyou@gmail.com>
2026-03-10 11:58:57 +0000

    fixed the launch that was loading 2025 world

--- turtlebot3_gazebo/launch/turtlebot3_task_world_2026.launch.py
+++ turtlebot3_gazebo/launch/turtlebot3_task_world_2026.launch.py
@@ -22,7 +22,7 @@ def generate_launch_description():
     world = os.path.join(
         get_package_share_directory('turtlebot3_gazebo'),
         'worlds',
-        'task_world_2025.world'
+        'task_world_2026.world'
     )
 
     gzserver_cmd = IncludeLaunchDescription(
